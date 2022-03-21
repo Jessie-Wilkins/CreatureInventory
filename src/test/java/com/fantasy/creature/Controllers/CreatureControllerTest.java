a2c98dae7ea0f9b9993255915651f8cb78a87bcb
Jessie-Wilkins <wilkinsj@fontbonne.edu>
2022-03-21 17:12:21 -0500

    Added Controller test, but need to still create CreatureRepository

---
+++ src/test/java/com/fantasy/creature/Controllers/CreatureControllerTest.java
@@ -0,0 +1,34 @@
+package com.fantasy.creature.Controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import com.fantasy.creature.Entities.Creature;
+
+public class CreatureControllerTest {
+	
+//	@Test
+//	public void testThatGetCreatureGetsAllCreatures() {
+//		CreatureController creatureController = new CreatureController();
+//		Creature creature = mock(Creature.class);
+//		when(creature.getId()).thenReturn(1L);
+//		when(creature.getDexterity()).thenReturn(5.2);
+//		when(creature.getEndurance()).thenReturn(8.9);
+//		when(creature.getHeight()).thenReturn(6.2);
+//		when(creature.getName()).thenReturn("Griffin");
+//		when(creature.getIntelligence()).thenReturn(2.3);
+//		when(creature.getResistance()).thenReturn(9.0);
+//		when(creature.getSpeed()).thenReturn(7.0);
+//		when(creature.getStrength()).thenReturn(14.7);
+//		when(creature.getVitality()).thenReturn(50.1);
+//		when(creature.getWeight()).thenReturn(300.93);
+//		assertEquals(creatureController);
+//	}
+
+}
